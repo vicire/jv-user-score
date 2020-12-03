99a6cf6819ecd360ca597e91e558d1d213b6abce
Roman Saviak <saviak.roman@gmail.com>
2020-12-03 20:11:13 +0200

    Make changes with split

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,13 +5,11 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        String[][] che = new String[records.length][1];
-        for (int i = 0; i < records.length; i++) {
-            che[i] = records[i].split(":");
-        }
-        for (String[] check : che) {
-            if (check[0].equals(email)) {
-                return (Integer.parseInt(check[1]));
+        String [] divide;
+        for (String record : records) {
+            divide = record.split(":");
+            if (divide[0].equals(email)) {
+                return (Integer.parseInt(divide[1]));
             }
         }
         throw new UserNotFoundException("User with given email doesn't exist");
