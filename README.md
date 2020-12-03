e66819996dc3edeefe38db8a3c8c84333e4696fd
Roman Saviak <saviak.roman@gmail.com>
2020-12-03 19:19:58 +0200

    Make changes with split and reductant method

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Method receives two parameters: arrays of strings `records` and single string el
 email and score stored as single string, so record looks like this : "someEmail@gmail.com:userScore". 
 `userScore` is in integer format and has value in range `0` - `Integer.MAX_VALUE`;
 You task here is to find out if records contains user with certain email that you 
-receive, if user exists return his score in int format, if not, throw core.basesyntax.exception 
+receive, if user exists return his score in int format, if not, throw exception 
 `UserNotFoundException` with message "User with given email doesn't exist"
 (since it's our custom logic let's create our own, unchecked, core.basesyntax.exception).
  
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,18 +5,15 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        if (makeResult(records, email) < 0) {
-            throw new UserNotFoundException("User with given email doesn't exist");
+        String[][] che = new String[records.length][1];
+        for (int i = 0; i < records.length; i++) {
+            che[i] = records[i].split(":");
         }
-        return (makeResult(records, email));
-    }
-
-    public int makeResult(String[] records, String email) {
-        for (String record : records) {
-            if (record.substring(0, record.indexOf(":")).equals(email)) {
-                return (Integer.parseInt(record.substring(record.indexOf(":") + 1)));
+        for (String[] check : che) {
+            if (check[0].equals(email)) {
+                return (Integer.parseInt(check[1]));
             }
         }
-        return (-1);
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
